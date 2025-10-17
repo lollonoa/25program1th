25456c1ebc6650c2a281253664d2219ee03c496a
lollonoa <tlgja0221@gmail.com>
2025-10-17 19:45:19 +0900

    2025.10.17. 원형 연결 리스트

--- 25proarma1th/25proarma1th/소스.cpp
+++ 25proarma1th/25proarma1th/소스.cpp
@@ -20,12 +20,104 @@ public:
 		head = nullptr;
 		tail = nullptr;
 	}
+
+	void push_front(T data)
+	{
+		Node* newNode=new Node;
+		newNode->data = data;
+		newNode->next = nullptr;
+		newNode->previous = nullptr;
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			head->previous = newNode;
+			newNode->next = head;
+			head = newNode;
+		}
+		size++;
+	}
+
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "linked list is empty" << endl;
+			return;
+		}
+		else
+		{
+			Node* deleteNode = head;
+
+			if (head == tail)
+			{
+				head = nullptr;
+				tail = nullptr;
+			}
+			else
+			{
+				deleteNode->next->previous = nullptr;
+				head = head->next;
+
+			}
+			delete deleteNode;
+
+			size--;
+		}
+	}
+
+	void push_back(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = nullptr;
+		newNode->previous - nullptr;
+
+		if (tail == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			newNode->previous = tail;
+			tail = newNode;
+		}
+		size++;
+	}
+
+	void pop_back()
+	{
+		Node* deleteNode = head;
+		head->next = nullptr;
+		tail = nullptr;
+		if (head == nullptr)
+		{
+			cout << "linked list is empty" << endl;
+			return;
+		}
+
+	}
 };
 
 int main()
 {
 	List<int> list;
+	list.push_front(10);
+	list.push_front(5);
+
+	list.push_back(10);
+	list.push_back(20);
+
+	list.pop_front();
+	list.pop_front();
 
 
+
+	
 	return 0;
 }
