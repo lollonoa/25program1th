f667cfed0a966de923e44e80eef70e9555060a8f
lollonoa <tlgja0221@gmail.com>
2025-10-20 19:44:17 +0900

    2025.10.20. 원형컨테이너

--- 25proarma1th/25proarma1th/소스.cpp
+++ 25proarma1th/25proarma1th/소스.cpp
@@ -5,119 +5,86 @@ class List
 private:
 	struct Node
 	{
-		Node* previous;
 		T data;
 		Node* next;
 	};
 
 	int size;
 	Node* head;
-	Node* tail;
+
 public:
 	List()
 	{
 		size = 0;
 		head = nullptr;
-		tail = nullptr;
 	}
-
-	void push_front(T data)
-	{
-		Node* newNode=new Node;
-		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous = nullptr;
-		if (head == nullptr)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else
-		{
-			head->previous = newNode;
-			newNode->next = head;
-			head = newNode;
-		}
-		size++;
-	}
-
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "linked list is empty" << endl;
-			return;
-		}
-		else
-		{
-			Node* deleteNode = head;
-
-			if (head == tail)
-			{
-				head = nullptr;
-				tail = nullptr;
-			}
-			else
-			{
-				deleteNode->next->previous = nullptr;
-				head = head->next;
-
-			}
-			delete deleteNode;
-
-			size--;
-		}
-	}
-
 	void push_back(T data)
 	{
 		Node* newNode = new Node;
 		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous - nullptr;
-
-		if (tail == nullptr)
+		
+		if (head == nullptr)
 		{
 			head = newNode;
-			tail = newNode;
+			newNode->next = head;
 		}
 		else
 		{
-			tail->next = newNode;
-			newNode->previous = tail;
-			tail = newNode;
+			newNode->next = head->next;
+			head->next = newNode;
+			head = newNode;
+		}
+	size++;
+	}
+	void push_front(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->next = head;
+		}
+		else
+		{
+			newNode->next = head->next;
+
+			head->next = newNode;
 		}
 		size++;
 	}
-
-	void pop_back()
+	void pop_front()
 	{
-		Node* deleteNode = head;
-		head->next = nullptr;
-		tail = nullptr;
+		Node* newNode = new Node;
+		newNode->data = data;
+		
 		if (head == nullptr)
 		{
 			cout << "linked list is empty" << endl;
 			return;
 		}
-
+		else
+		{
+			Node* deleteNode = newNode;
+			head->next = newNode;
+			newNode->next = head->next;
+		}
+		size--;
 	}
 };
 
 int main()
 {
 	List<int> list;
-	list.push_front(10);
-	list.push_front(5);
 
 	list.push_back(10);
 	list.push_back(20);
 
+	list.push_front(5);
+	list.push_front(1);
+
 	list.pop_front();
-	list.pop_front();
 
-
-
-	
 	return 0;
 }
