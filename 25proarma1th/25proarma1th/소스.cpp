3e054ab38341d846581978088b80af57e2260b5c
lollonoa <tlgja0221@gmail.com>
2025-10-29 19:46:11 +0900

    2025.10.29. 인접 행렬

--- 25proarma1th/25proarma1th/소스.cpp
+++ 25proarma1th/25proarma1th/소스.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-template<typename KEY, typename VALUE>
+template<typename KEY, typename VALEU>
 
 class HashTable
 {
@@ -7,11 +7,12 @@ private:
 	struct Node
 	{
 		KEY key;
-		VALUE value;
+		VALEU value;
 		Node* next;
 	};
 	struct Bucket
 	{
+		int count;
 		Node* head;
 	};
 	int size;
@@ -22,15 +23,48 @@ public:
 	HashTable()
 	{
 		size = 0;
-		capacity = 0;
+		capacity = 8;
 
-		bucket = nullptr;
+		bucket = new Bucket [capacity];
+
+		for (int i = 0; i < capacity; i++)
+		{
+			bucket[i].head = nullptr;
+			bucket[i].count = 0;
+		}
+	}
+	//unsigned int 정수만 나오는 함수
+	template<typename KEY>
+	unsigned int hash_function(KEY key)
+	{
+	
+		return (unsigned int)key % capacity;
+	}
+	template<>
+	unsigned int hash_function(const char * key)
+	{	
+		int sum = 0;
+		for (int i = 0; *key != '\0'; i++)
+		{
+			sum += key[i];
+			key = key + 1;
+		}
+
+		return (unsigned int)sum % capacity;
+	}
+
+	void insart(KEY key, VALEU value)
+	{
+
 	}
 };
 
 int main()
 {
+	HashTable < const char*, int> hashtable;
 
-
+	cout << hashtable.hash_function("Korea") << endl;
+	cout << hashtable.hash_function("Brazil") << endl;
+	cout << hashtable.hash_function("China") << endl;
 	return 0;
 }
